7e2bc395f3bfc0d1596c161a6a314eb3de2a5619
Aloïs Masson--Claudez <alois.mc@hotmail.com>
2022-05-10 09:41:22 +0200

    retrait de l'affichage du tableau

--- fichiers/les différentes pages/fichier html/submit_connexion.php
+++ fichiers/les différentes pages/fichier html/submit_connexion.php
@@ -48,16 +48,6 @@
                 }
             ?>
         </p>
-        <?php
-            echo "<pre>";
-            print_r($_POST);
-            echo "</pre>";
-        ?>
-        <?php
-            echo "<pre>";
-            print_r($_FILES);
-            echo "</pre>"
-        ?>
         <?php include("footer.php"); ?>
     </body>
 </html>
