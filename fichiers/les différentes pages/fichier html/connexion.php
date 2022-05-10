072867809a68f45d6d889d8db382b2565d34ddaa
Aloïs Masson--Claudez <alois.mc@hotmail.com>
2022-05-10 09:27:16 +0200

    ajout de l'envoi de fichiers

--- fichiers/les différentes pages/fichier html/connexion.php
+++ fichiers/les différentes pages/fichier html/connexion.php
@@ -11,14 +11,22 @@
     </head>
     <body>
         <?php include("header.php"); ?>
-        <form method="get" action="submit_connexion.php">
+        <form method="post" action="submit_connexion.php" enctype="multipart/form-data">
             <p>
-                <input type="text" name="name" id="name" placeholder="Nom" maxlength="256">
-                <input type="text" name="firstname" id="firstname" placeholder="Prénom" maxlength="256">
+                <label for="name">Nom :</label>
+                <input type="text" name="name" id="name" placeholder="Nom">
+                <label for="firstname">Prénom :</label>
+                <input type="text" name="firstname" id="firstname" placeholder="Prénom">
             </p>
             <p>
-                <input type="email" name="email" id="email" placeholder="E-mail" maxlength="256">
-                <input type="password" name="password" id="password" placeholder="Mot de passe" maxlength="256">
+                <label for="email">E-mail :</label>
+                <input type="email" name="email" id="email" placeholder="E-mail">
+                <label for="password">Mot de passe :</label>
+                <input type="password" name="password" id="password" placeholder="Mot de passe">
+            </p>
+            <p>
+                <label for="pictureFile">Photo de profil :</label><br>
+                <input type="file" name="pictureFile" id="pictureFile">
             </p>
             <p>
                 <button type="submit">Envoyer</button>
--- fichiers/les différentes pages/fichier html/submit_connexion.php
+++ fichiers/les différentes pages/fichier html/submit_connexion.php
@@ -1,25 +1,63 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <meta charset="UTF-8">
+    <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Connecté</title>
+        <title>Option CAV | connecté</title>
+        <link rel="stylesheet" href="../fichier css/styles.css">
+        <link rel="stylesheet" media="screen and (max-width: 500px)" href="../fichier css/phone_styles.css">
+        <link rel="icon" type="image/png" href="../../../fichiers/images/logo.png">
     </head>
     <body>
+        <?php include("header.php"); ?>
         <?php
             if (
-                isset($_GET["name"])
-                && isset($_GET["firstname"])
-                && isset($_GET["email"])
-                && isset($_GET["password"])
-                && filter_var($_GET["email"], FILTER_VALIDATE_EMAIL)
+                isset($_POST["name"])
+                && isset($_POST["firstname"])
+                && isset($_POST["email"])
+                && isset($_POST["password"])
+                && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)
                 ) {
-                    echo "<p>Bonjour " . $_GET["firstname"] . " " . $_GET["name"] . ". </p>";
-                    echo "<p>Votre adresse e-mail est : " . $_GET["email"] . " et votre mot de passe est : " . $_GET["password"] . ". </p>";
+                    echo "<p class='pute'>Bonjour " . strip_tags($_POST["firstname"]) . " " . strip_tags($_POST["name"]) . ". </p>";
+                    echo "<p class='pute'>Votre adresse e-mail est : " . strip_tags($_POST["email"]) . " et votre mot de passe est : " . strip_tags($_POST["password"]) . ". </p>";
                 } else {
-                    echo "Veuillez remplir tous les champs et utiliser une adresse e-mail valide. ";
+                    echo "<p class='pute'>Veuillez remplir tous les champs et utiliser une adresse e-mail valide. </p>";
                 }
         ?>
+        <p class="pute">
+            <?php
+                if (isset($_FILES["pictureFile"]["type"]) && $_FILES["pictureFile"]["error"] == 0) {
+                    if ($_FILES["pictureFile"]["size"] <= 1 * 10**6) {
+                        $fileInfo = pathinfo($_FILES["pictureFile"]["name"]);
+                        $fileType = $fileInfo["extension"];
+                        $allowedType = ["png", "jpg", "jpeg", "gif"];
+
+                        if (in_array($fileType, $allowedType)) {
+                            echo "Tout est bon. <br>";
+                            move_uploaded_file($_FILES["pictureFile"]["tmp_name"], "../../uploads/" . $_FILES["pictureFile"]["name"]);
+                            echo "<img class=\"pute\" src=\"../../uploads/" . $_FILES["pictureFile"]["name"] . "\" alt=\"photo de profil\">";
+                        } else {
+                            echo "Veuillez envoyer uniquement des images au format png, jpg, jpeg ou gif. ";
+                        }
+                    } else {
+                        echo "Le fichier est trop volumineux (supérieur à 1 Mo). ";
+                    }
+                } else {
+                    echo "Veuillez insérer une photo de profil. ";
+                }
+            ?>
+        </p>
+        <?php
+            echo "<pre>";
+            print_r($_POST);
+            echo "</pre>";
+        ?>
+        <?php
+            echo "<pre>";
+            print_r($_FILES);
+            echo "</pre>"
+        ?>
+        <?php include("footer.php"); ?>
     </body>
 </html>
