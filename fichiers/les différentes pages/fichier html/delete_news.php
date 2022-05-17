010742d3515b4cc9d5c30b2864552cd6f3180b17
Aloïs Masson--Claudez <alois.mc@hotmail.com>
2022-05-17 22:25:11 +0200

    correction du titre des pages

--- fichiers/les différentes pages/fichier html/delete_news.php
+++ fichiers/les différentes pages/fichier html/delete_news.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Option CAV | plus d'infos</title>
+        <title>Option CAV | article supprimé</title>
         <link rel="stylesheet" href="../fichier css/styles.css">
         <link rel="stylesheet" media="screen and (max-width: 500px)" href="../fichier css/phone_styles.css">
         <link rel="icon" type="image/png" href="../../../fichiers/images/logo.png">
